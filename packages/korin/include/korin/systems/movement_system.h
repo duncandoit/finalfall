829c0971f986d079bcb5246b2f7ce9ae99cae082
Zachary Duncan <zachary.duncan@icloud.com>
2024-07-18 02:33:27 -0400

    Added a true component to demo movement of an entity

--- packages/korin/include/korin/systems/movement_system.h
+++ packages/korin/include/korin/systems/movement_system.h
@@ -1,23 +1,47 @@
-// // movement_system.h
-// //
-// // Brief description
-// //
-// // Copyright (c) Zachary Duncan - Duncandoit
-// // 2024-07-09
+// movement_system.h
+//
+// Brief description
+//
+// Copyright (c) Zachary Duncan - Duncandoit
+// 2024-07-09
 
-// #pragma once
+#pragma once
 
-// #include <unordered_map>
-// #include "korin/entity.h"
-// #include "korin/system.h"
+#include <unordered_map>
+#include <iostream>
 
-// namespace korin
-// {
-// struct TransformComponent;
-// struct VelocityComponent;
-// class MovementSystem : public System {
-// public:
-//       void update(float ts) override;
-//       // void notify(ComponentPtr component) override;
-// };
-// }
+#include "korin/system.h"
+#include "korin/component.h"
+#include "korin/components/transform_component.h"
+
+namespace korin
+{
+class MovementSystem : public System
+{
+public:
+   // Request the TransformComponent type
+   virtual ComponentTypeID primaryComponentTypeID() const override
+   {
+      return Component::typeID<TransformComponent>(); 
+   }
+
+   virtual void notify(const ComponentPtr& component) override
+   {
+      // Do nothing
+   }
+
+   // Update method to move the TransformComponents
+   virtual void update(float ts, const ComponentPtr& component) override
+   {
+      KORIN_ASSERT(component.typeID() == Component::typeID<TransformComponent>());
+      
+      auto transform = std::static_pointer_cast<TransformComponent>(component);
+      if (transform)
+      {
+         transform->x += 1.0f * ts;
+         transform->y += 1.0f * ts;
+         std::cout << "Entity Position: (" << transform->x << ", " << transform->y << ")" << std::endl;
+      }
+   }
+};
+} // namespace korin
