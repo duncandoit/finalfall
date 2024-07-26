8895757ddaf4214c6ed3c7d66e91a66ff6f49017
Zachary Duncan <zachary.duncan@icloud.com>
2024-07-26 10:52:20 -0400

    Static initialization in cpp

---
+++ packages/korin/src/entity.cpp
@@ -0,0 +1,11 @@
+// entity.cpp
+//
+// Copyright (c) Zachary Duncan - Duncandoit
+// 07/23/2024
+
+#include "korin/entity.h"
+
+namespace korin
+{
+EntityID Entity::m_NextID = 0;
+} // namespace korin
