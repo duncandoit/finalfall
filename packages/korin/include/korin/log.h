bf81a4b8a8dd9ac23910fde9d6fdc41bb4362444
Zachary Duncan <zachary.duncan@icloud.com>
2024-09-16 17:56:45 -0400

    Reverting a git mistake that switch the log.h file with the Korin premake5.lua file

--- packages/korin/include/korin/log.h
+++ packages/korin/include/korin/log.h
@@ -1,214 +1,41 @@
-KORIN_DIR = path.getabsolute('..')
+// log.h
+//
+// Copyright (c) Zachary Duncan - Duncandoit
+// 09/13/2024
 
-workspace 'korin'
-    startproject 'korin'
-    configurations {'debug', 'release'}
-    location (KORIN_DIR ..'/build')
+#pragma once
 
--- The generated library will use this name and append 'lib'
-project 'korin'
-do
-    kind 'StaticLib'
-    language 'C++'
-    cppdialect 'C++17'
-    toolset 'clang'
-    location '.'
-    targetdir (KORIN_DIR .. '/build/%{cfg.system}/bin/%{cfg.buildcfg}')
-    objdir (KORIN_DIR .. '/build/%{cfg.system}/obj/%{cfg.buildcfg}')
-    files 
-    {
-        KORIN_DIR .. '/include/**.h',
-        KORIN_DIR .. '/src/**.cpp',
-    }
-    includedirs 
-    {
-        KORIN_DIR .. '/include',                                  -- Korin
-        -- KORIN_DIR ..'/dependencies/%{cfg.system}/glfw/include',  -- GLFW
-        KORIN_DIR .. '/dependencies/submodules/spdlog/include'    -- spdlog
-    }
-    libdirs 
-    {
-        -- KORIN_DIR ..'/dependencies/%{cfg.system}/glfw', -- GLFW
-        KORIN_DIR .. '/dependencies/submodules/spdlog/build'   -- spdlog
-    }
-    links 
-    {
-        -- 'glfw3',           -- GLFW
-        'spdlog'           -- spdlog
-    }
-    buildoptions 
-    {
-        '-Wall',                     -- Enable all warnings
-        '-fno-rtti',                 -- Disable RTTI
-        '-Werror=format',            -- Treat format errors as errors
-        '-Wimplicit-int-conversion', -- Implicit int conversion
-    startproject 'korin'
-    configurations {'debug', 'release'}
-    location '../build'
+#include <memory>
 
--- dofile(path.join(path.getabsolute('../dependencies/'), 'premake5_dependency.lua'))
+#include "korin/core.h"
+#include "spdlog/spdlog.h"
 
--- The generated library will use this name and append 'lib'
-project 'korin'
-do
-    kind 'StaticLib'
-    language 'C++'
-    cppdialect 'C++17'
-    toolset 'clang'
-    location '.'
-    targetdir '../build/%{cfg.system}/bin/%{cfg.buildcfg}'
-    objdir '../build/%{cfg.system}/obj/%{cfg.buildcfg}'
+namespace korin
+{
+class KORIN_API Log
+{
+public:
+   static void init();
 
-    files 
-    {
-        '../include/**.h',
-        '../src/**.cpp'
-    }
+   inline static std::shared_ptr<spdlog::logger>& coreLogger() { return s_CoreLogger; }
+   inline static std::shared_ptr<spdlog::logger>& clientLogger() { return s_ClientLogger; }
 
-    includedirs 
-    {
-        '../include',
-        '../dependencies/%{cfg.system}/glfw/include',
-    }
+private:
+   static std::shared_ptr<spdlog::logger> s_CoreLogger;
+   static std::shared_ptr<spdlog::logger> s_ClientLogger;
+};
+} // namespace korin
 
-    libdirs 
-    {
-        '../dependencies/%{cfg.system}/glfw' -- GLFW
-    }
+// Core log macros
+#define KORIN_CORE_ERROR(...) ::korin::Log::coreLogger()->error(__VA_ARGS__)
+#define KORIN_CORE_WARN(...)  ::korin::Log::coreLogger()->warn(__VA_ARGS__)
+#define KORIN_CORE_INFO(...)  ::korin::Log::coreLogger()->info(__VA_ARGS__)
+#define KORIN_CORE_TRACE(...) ::korin::Log::coreLogger()->trace(__VA_ARGS__)
+#define KORIN_CORE_FATAL(...) ::korin::Log::coreLogger()->fatal(__VA_ARGS__)
 
-    links 
-    {
-        'glfw3'              -- GLFW
-    }
-
-    buildoptions 
-    {
-        '-Wall',                     -- Enable all warnings
-        '-fno-exceptions',           -- Disable exceptions
-        '-fno-rtti',                 -- Disable RTTI
-        '-Werror=format',            -- Treat format errors as errors
-        '-Wimplicit-int-conversion', -- Implicit int conversion
-        '-Werror=vla'                -- Treat variable length arrays as errors
-    }
-
-    filter {'system:macosx'}
-    do
-        links 
-        { 
-            'CoreFoundation.framework',
-            'Cocoa.framework',
-            'IOKit.framework',
-            'CoreVideo.framework',
-            'CoreGraphics.framework', 
-            'OpenGL.framework',
-        }
-        defines {'KORIN_PLATFORM_MACOSX', 'KORIN_BUILD_DLL'}
-        buildoptions 
-        {
-            '-Wimplicit-float-conversion'
-        }
-    end
-
-    filter {'system:macosx', 'configurations:release'}
-    do
-        buildoptions {'-flto=full'}
-    end
-
-    -- filter 'system:windows'
-    -- do
-    --     links
-    --     {
-    --         '../dependencies/windows/glfw/glfw3.lib'
-    --     }
-    --     architecture 'x64'
-    --     defines {'_USE_MATH_DEFINES'}
-    --     flags {'FatalCompileWarnings'}
-    --     buildoptions {WINDOWS_CLANG_CL_SUPPRESSED_WARNINGS}
-    --     staticruntime 'on'
-    --     runtime 'Release'
-    --     removebuildoptions 
-    --     {
-    --         '-fno-exceptions',
-    --         '-fno-rtti'
-    --     }
-    -- end
-
-    -- filter {'system:ios'}
-    -- do
-    --     links { "CoreGraphics.framework", "Cocoa.framework" }
-    --     buildoptions {'-flto=full'}
-    -- end
-
-    -- filter {'system:ios', 'options:variant=system'}
-    -- do
-    --     buildoptions 
-    --     {
-    --         '-mios-version-min=10.0 -fembed-bitcode -arch armv7 -arch arm64 -arch arm64e -isysroot ' ..
-    --             (os.getenv('IOS_SYSROOT') or '')
-    --     }
-    -- end
-
-    -- filter {'system:ios', 'options:variant=emulator'}
-    -- do
-    --     buildoptions 
-    --     {
-    --         -- '-mios-version-min=10.0 -arch arm64 -arch x86_64 -arch i386 -isysroot ' .. (os.getenv('IOS_SYSROOT') or '')
-    --         '-mios-version-min=10.0 -arch x86_64 -isysroot ' .. (os.getenv('IOS_SYSROOT') or '')
-    --     }
-    --     targetdir '%{cfg.system}_sim/bin/%{cfg.buildcfg}'
-    --     objdir '%{cfg.system}_sim/obj/%{cfg.buildcfg}'
-    -- end
-
-    -- filter {'system:android', 'configurations:release'}
-    -- do
-    --     buildoptions {'-flto=full'}
-    -- end
-
-    -- filter {'system:android', 'options:arch=${cfg.architecture}'}
-    -- do
-    --     targetdir '%{cfg.system}/${cfg.architecture}/bin/%{cfg.buildcfg}'
-    --     objdir '%{cfg.system}/${cfg.architecture}/obj/%{cfg.buildcfg}'
-    -- end
-
-    filter 'configurations:debug'
-    do
-        defines {'DEBUG'}
-        symbols 'On'
-    end
-
-    filter 'configurations:release'
-    do
-        defines {'RELEASE'}
-        defines {'NDEBUG'}
-        optimize 'On'
-    end
-end
-
--- newoption 
--- {
---     trigger = 'variant',
---     value = 'type',
---     description = 'Choose the variant for iOS builds.',
---     allowed = 
---     {
---         {'system', 'Builds the static library for the provided system'},
---         {'emulator', 'Builds for an emulator/simulator for the provided system'}
---     },
---     default = 'system'
--- }
-
--- newoption 
--- {
---     trigger = 'arch',
---     value = 'ABI',
---     description = 'Choose the architecture for Android builds.',
---     allowed = 
---     {
---         {'x86'},
---         {'x64'},
---         {'arm'},
---         {'arm64'}
---     }
--- }
-
--- os.chdir("../scripts")
+// Client log macros
+#define KORIN_ERROR(...) ::korin::Log::clientLogger()->error(__VA_ARGS__)
+#define KORIN_WARN(...)  ::korin::Log::clientLogger()->warn(__VA_ARGS__)
+#define KORIN_INFO(...)  ::korin::Log::clientLogger()->info(__VA_ARGS__)
+#define KORIN_TRACE(...) ::korin::Log::clientLogger()->trace(__VA_ARGS__)
+#define KORIN_FATAL(...) ::korin::Log::clientLogger()->fatal(__VA_ARGS__)
